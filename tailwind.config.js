3da074af11f86ecb619d57412a92ba33fad42e50
gogakazarenko-glitch <gogakazarenko@gmail.com>
2026-02-20 18:28:41 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0f172a",
+        accent: "#38bdf8",
+      },
+    },
+  },
+  plugins: [],
+}
